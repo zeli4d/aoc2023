0f1ba3dc5f7c108ac8faa889f64b2604ff3b637c
zelenym <martin.zeleny@betsys.com>
2023-12-04 00:32:52 +0100

    aoc01b oneight issue

--- src/aoc01.rs
+++ src/aoc01.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::env;
 use std::io::{self, BufRead};
 use std::fs::File;
@@ -8,13 +9,39 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let input_path = &args[1];
+    let number = HashMap::from([
+        ("0", 0),
+        ("1", 1),
+        ("2", 2),
+        ("3", 3),
+        ("4", 4),
+        ("5", 5),
+        ("6", 6),
+        ("7", 7),
+        ("8", 8),
+        ("9", 9),
+        ("zero", 0),
+        ("one", 1),
+        ("two", 2),
+        ("three", 3),
+        ("four", 4),
+        ("five", 5),
+        ("six", 6),
+        ("seven", 7),
+        ("eight", 8),
+        ("nine", 9),
+    ]);
 
-    println!("Input file: {}", input_path);
 
-    let re = Regex::new(r"[0-9]").unwrap();
+    // println!("Input file: {}", input_path);
+
+    let re = Regex::new(r"[0-9]|zero|one|two|three|four|five|six|seven|eight|nine").unwrap();
     // let mut results = vec![];
 
     let mut suma: i32 = 0;
+    let mut first: &str;
+    let mut last: &str;
+    let mut num: i32;
 
     if let Ok(lines) = read_lines(input_path) {
         // Consumes the iterator, returns an (Optional) String
@@ -25,13 +52,27 @@ fn main() {
 
                 let caps:Vec<_> = re.find_iter(ip.as_str()).map(|mat| mat.as_str()).collect();
 
-                // print!(" {:?}", caps);
-                suma += format!("{}{}", caps.first().unwrap(),  caps.last().unwrap()).parse::<i32>().unwrap();
+                first = "0";
+                last ="0";
 
-                println!("{} = {}", ip, suma);
+                if caps.len() == 1 {
+                    last = caps.last().unwrap();
+                } else if caps.len() > 1 {
+                    first = caps.first().unwrap();
+                    last = caps.last().unwrap();
+                }
+
+//todo: oneight
+                // print!(" {:?}", caps);
+                num = format!("{}{}", number.get(first).unwrap(), number.get(last).unwrap()).parse::<i32>().unwrap();
+                suma += num;
+
+                println!("{} {}, {} = {}", num, caps.first().unwrap(), caps.last().unwrap(), ip);
             }
         }
     }
+
+    println!("celkem: {}", suma);
 
 }
 
